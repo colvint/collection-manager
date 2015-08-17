1f4046fcb68bb99e4c8084ef42862f2fef1925aa
Taurus Colvin <taurus.colvin@gmail.com>
2015-08-17 11:54:33 -0400

    specify package depencies in prep for publishing

--- package.js
+++ package.js
@@ -9,8 +9,15 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
 
-  api.use('reactjs:react', 'client');
-  api.use('firfi:meteor-react-bootstrap', 'client');
+  api.use('reactjs:react@0.2.4', 'client');
+  api.use('firfi:meteor-react-bootstrap@0.0.5', 'client');
+  api.use('maxharris9:classnames@0.0.1', 'client');
+  api.use('fortawesome:fontawesome@4.3.0', 'client');
+
+  api.imply('reactjs:react');
+  api.imply('firfi:meteor-react-bootstrap');
+  api.imply('maxharris9:classnames');
+  api.imply('fortawesome:fontawesome');
 
   api.export('CollectionManagerMixin', 'client');
 
