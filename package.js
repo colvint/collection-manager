ba7baade3ebc181af011d6496f57ebbca3a0b2c7
Taurus Colvin <taurus.colvin@gmail.com>
2015-08-23 14:10:37 -0400

    vup

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'tauruscolvin:collection-manager',
-  version: '0.0.7',
+  version: '0.0.8',
   summary: 'A drop-in collection manager for your client.',
   git: 'git@github.com:colvint/collection-manager.git',
   documentation: 'README.md'
