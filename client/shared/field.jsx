15703443e7c4e276b9ebc6af546f68887ed8335b
Taurus Colvin <taurus.colvin@gmail.com>
2016-02-10 17:10:33 -0500

    more bounce on tinymce plugins

--- client/shared/field.jsx
+++ client/shared/field.jsx
@@ -57,8 +57,18 @@ var RichTextField = React.createClass({
           {...this.props}
           content={this.props.value}
           config={{
-            plugins: 'autolink link image lists print preview',
-            toolbar: 'undo redo | bold italic | alignleft aligncenter alignright'
+            theme: 'modern',
+            height: 300,
+            plugins: [
+              'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+              'searchreplace wordcount visualblocks visualchars code fullscreen',
+              'insertdatetime media nonbreaking save table contextmenu directionality',
+              'emoticons template paste textcolor colorpicker textpattern imagetools'
+            ],
+            toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+            toolbar2: 'print preview media | forecolor backcolor emoticons',
+            image_advtab: true,
+            images_upload_handler: TinyMCEUpload.handler,
           }} />
       </div>
     );
